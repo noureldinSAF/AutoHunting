8d4c64747037312f2c1c04a88e683186c4b0aa3c
noureldinSAF <eldeennoor207@gmail.com>
2026-01-03 22:49:21 +0300

    Description of what I changed

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Noureldin/AutoHunting/pkg/scanner"
+)
+
+func main() {
+	fmt.Println("Hello, Bug Bounty!")
+	fmt.Println("Updated by Noureldin")
+	scanner.Scan()
+}
