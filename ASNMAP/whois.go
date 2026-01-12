2bad7076c9b3332274a8c7739163b51f70d580af
Noureldin <eldeennoor207@gmail.com>
2026-01-12 11:51:21 +0000

    first setup

--- ASNMAP/whois.go
+++ ASNMAP/whois.go
@@ -1,8 +1,28 @@
 package main 
 import (
 	"fmt"
+	"github.com/likexian/whois"
+	"github.com/likexian/whois-parser"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	query := "google.com"
+
+	result, err := whois.Whois(query)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	parsedResult, err := whoisparser.Parse(result)
+	if err != nil {
+		fmt.Println("Error parsing whois data:", err)
+		return
+	}
+
+	// Print parsed whois information
+
+	fmt.Println(parsedResult.Registrant.Organization)
+	
+
 }
