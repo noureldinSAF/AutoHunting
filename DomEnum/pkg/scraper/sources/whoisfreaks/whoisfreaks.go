fbf567f9e0780bbef8827349a1c1732e40d37fac
noureldinSAF <eldeennoor207@gmail.com>
2026-01-21 14:24:09 +0300

    Add project files

--- DomEnum/pkg/scraper/sources/whoisfreaks/whoisfreaks.go
+++ DomEnum/pkg/scraper/sources/whoisfreaks/whoisfreaks.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+
 	//"log"
 	"math/rand"
 	"net/http"
@@ -14,19 +15,17 @@ import (
 	"github.com/cyinnove/logify"
 )
 
-
-type Source struct{
+type Source struct {
 	apiKeys []string
 }
 
 type responseObj struct {
-     TotalResult int `json:"total_Result"`
-	 TotalPages int `json:"total_Pages"`
-	 CurrentPage int `json:"current_Page"`
-	 Domains  []struct{
+	TotalResult int `json:"total_Result"`
+	TotalPages  int `json:"total_Pages"`
+	CurrentPage int `json:"current_Page"`
+	Domains     []struct {
 		DomainName string `json:"domain_name"`
 	} `json:"whois_domains_historical"`
-
 }
 
 func New(apiKeys []string) *Source {
@@ -45,67 +44,63 @@ func (s *Source) randomKey() string {
 	return s.apiKeys[rand.Intn(len(s.apiKeys))]
 }
 
-
 func (s *Source) Search(query string, client *http.Client) ([]string, error) {
-    var domains []string
+	var domains []string
 	if s.apiKeys == nil || len(s.apiKeys) == 0 {
 		return []string{}, fmt.Errorf("No API keys provided for whoisfreaks")
 	}
 
-	page := 1 
-	
+	page := 1
+
 	for {
 
-	fmtURL := fmt.Sprintf("https://api.whoisfreaks.com/v1.0/whois?apiKey=%s&whois=reverse&keyword=%s&page=%d", s.randomKey(), url.QueryEscape(query), page)
-	
-	req, err := http.NewRequest("GET", fmtURL, nil)
-	if err != nil {
-		return nil, err
-	}
+		fmtURL := fmt.Sprintf("https://api.whoisfreaks.com/v1.0/whois?apiKey=%s&whois=reverse&keyword=%s&page=%d", s.randomKey(), url.QueryEscape(query), page)
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-    body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode < 200 || resp.StatusCode > 300 {
-		return nil, fmt.Errorf("api error: status code %d, body:%s", resp.StatusCode, string(body))
-	}
-
-	var ro responseObj
-	if err := json.Unmarshal(body, &ro); err != nil {
-		return nil, fmt.Errorf("failed to parse JSON: %w", err)
-	}
-
-	logify.Infof("whoisfreaks: page%d/%d got=%d domains  totoalResults=%d\n", ro.CurrentPage, ro.TotalPages, len(ro.Domains), ro.TotalResult )
-    
-	
-	for _, domain := range ro.Domains {
-		if domain.DomainName == "" {
-			continue
+		req, err := http.NewRequest("GET", fmtURL, nil)
+		if err != nil {
+			return nil, err
 		}
-		domains = append(domains, domain.DomainName)
-	}
 
-	if ro.CurrentPage >= 5 {
-		break
+		resp, err := client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		if resp.StatusCode < 200 || resp.StatusCode > 300 {
+			return nil, fmt.Errorf("api error: status code %d, body:%s", resp.StatusCode, string(body))
+		}
+
+		var ro responseObj
+		if err := json.Unmarshal(body, &ro); err != nil {
+			return nil, fmt.Errorf("failed to parse JSON: %w", err)
+		}
+
+		logify.Infof("whoisfreaks: page%d/%d got=%d domains  totoalResults=%d\n", ro.CurrentPage, ro.TotalPages, len(ro.Domains), ro.TotalResult)
+
+		for _, domain := range ro.Domains {
+			if domain.DomainName == "" {
+				continue
+			}
+			domains = append(domains, domain.DomainName)
+		}
+
+		if ro.CurrentPage >= 5 {
+			break
+		}
+		page++
+		time.Sleep(1 * time.Minute)
 	}
-	page++
-	time.Sleep(1 * time.Minute)
- }
 	return domains, nil
 }
-	
-
 
 // func main() {
 
-// 	logify.MaxLevel = logify.Info	
+// 	logify.MaxLevel = logify.Info
 
 // 	s := New([]string{"446dab4ffd6346168998058953c55852"})
 
@@ -117,4 +112,3 @@ func (s *Source) Search(query string, client *http.Client) ([]string, error) {
 
 // 	fmt.Println(domains)
 // }
-
